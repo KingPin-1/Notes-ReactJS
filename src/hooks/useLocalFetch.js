9eced4f46f67496e7a33a6d2f9386011463d11b9
Athar M <93383101+KingPin-1@users.noreply.github.com>
2023-08-28 04:51:40 +0530

    Changed initial state

--- src/hooks/useLocalFetch.js
+++ src/hooks/useLocalFetch.js
@@ -7,17 +7,7 @@ const useLocalFetch = (key) => {
 
     useEffect(() => {
         if (localStorage.getItem(key) === null) {
-            localStorage.setItem(
-                key,
-                JSON.stringify([
-                    {
-                        id: 2,
-                        title: '2 Posty 2 Furious 4 me',
-                        datetime: 'August 20,2023 6:35:24 PM',
-                        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!',
-                    },
-                ])
-            );
+            localStorage.setItem(key, JSON.stringify([]));
             setData([]);
         } else {
             const res = JSON.parse(localStorage.getItem(key));
